dbbec5a2c872564551277e92bed7b6be71137066
Bernardo Vieira <bernardo5@live.com.pt>
2017-09-20 23:46:02 +0100

    upload

--- examples/example2.py
+++ examples/example2.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
-x = tf.placeholder(tf.int64, shape=(2, 2))
-y = tf.placeholder(tf.int64, shape=(2, 2))
+x = [[1,2],[2,1]]
+y = [[2,1],[1,2]]
 z = tf.add(x, y)
-tx = [[1,2],[2,1]]
-ty = [[2,1],[1,2]]
-init = tf.initialize_all_variables()
 sess = tf.Session()
-sess.run(init)
-print(sess.run(z, feed_dict={x: tx, y: ty}))
+print(sess.run(z))
--- examples/example3.py
+++ examples/example3.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+
+def checkit(g):
+    return [[g[0],g[1]],[g[2],g[3]]]
+
 x = tf.placeholder(tf.int64, shape=(2, 2))
 y = tf.placeholder(tf.int64, shape=(2, 2))
 z = tf.add(x, y)
-tx = [[1,2],[2,1]]
-ty = [[2,1],[1,2]]
-init = tf.initialize_all_variables()
+tx = checkit(input('x')) #1,2,2,1
+ty = checkit(input('y')) #2,1,1,2
 sess = tf.Session()
-sess.run(init)
 print(sess.run(z, feed_dict={x: tx, y: ty}))
